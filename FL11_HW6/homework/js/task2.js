a3587dfae0228f2e9cf80731ab27b43ff05084d1
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-06 20:22:21 +0300

    change conditions for triangle

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,7 +1,7 @@
 let a= Number(window.prompt('Please enter side a for the triangle', '2'));
 let b= Number(window.prompt('Please enter side b for the triangle', '2'));
 let c= Number(window.prompt('Please enter side c for the triangle', '2'));
-if(a<=b+c && b<=a+c && c<=a+b){
+if(a<b+c && b<a+c && c<a+b){
     if(a===b && b===c && a===c){
         console.log('Equivalent triangle');
     }else if (a===b || b===c || a===c){
