0a20cae9cff7d130d699e473ec4dd590996d5f9f
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-08 22:58:09 +0300

    change logic for prize

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -3,19 +3,12 @@ let attempt = 3;
 let hello = confirm('Do you want to play a game?');
 let possibleprize = 100;
 let totalPrize = 0;
-let prizeList ={
-    3: 100,
-    2: 50,
-    1: 25
-};
 const increaseMaxPoint = 4;
 const increasePrizeList = 2;
-
-let i = 0;
+const freeAttempt = 3;
 if(hello){
     while(hello){
         let slot = Math.floor(Math.random()*maxPoint);
-        console.log(slot);
         let attempt = 3;
         while(attempt) {
             let numericValue = +prompt(`Choose a roulette pocket number from 0 to ${maxPoint}).
@@ -23,10 +16,15 @@ if(hello){
      Total prize: ${totalPrize}$
      Possible prize on current attempt:  ${possibleprize}$`);
             if(numericValue === slot){
-                totalPrize = totalPrize + prizeList[attempt];
+                if(attempt===freeAttempt){
+                    totalPrize = totalPrize + possibleprize;
+                }else if(attempt===increasePrizeList){
+                    totalPrize = totalPrize + possibleprize/increasePrizeList;
+                }else{
+                    totalPrize = totalPrize + possibleprize/increaseMaxPoint;
+                }
                 attempt--;
                 alert(`Congratulation, you won!   Your prize is:  ${(totalPrize)} $`);
-                console.log(totalPrize);
                 break;
             } else if(attempt===0){
                 alert(`Thank you for your participation. Your prize is: ${(totalPrize)} $`);
@@ -40,9 +38,7 @@ if(hello){
             alert(`Thank you for your participation. Your prize is: ${(totalPrize)} $`);
         }
         maxPoint = maxPoint + increaseMaxPoint;
-        prizeList = prizeList * increasePrizeList;
-        console.log(prizeList);
-
+        possibleprize = possibleprize * increasePrizeList;
     }
 } else {
     alert('You did not become a billionaire, but can.');
