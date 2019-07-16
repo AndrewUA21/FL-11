e21e3bbc07873f43427771ef583e73da0aa00591
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-16 12:50:53 +0300

    0 task ready

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+function getNumbers (data){
+
+    let arr = [];
+    for(let i = 0;i<data.length; i++){
+        let num = parseInt(data[i]);
+        if(!isNaN(num)){
+            arr.push(data[i]);
+        }
+   }
+    console.log(arr);
+}
+getNumbers('n1um3ber95');
