3dfc9ec5f4adb9e2292475a00c8189a683094e72
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-19 16:50:02 +0300

    battle for 2 fighters ready

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,8 +1,6 @@
 class Fighter {
     constructor(data) {
         this.fighter = data;
-        this.win = 0;
-        this.loss = 0;
         this.hp = data.hp;
         this.name = data.name;
         this.damage = data.damage;
@@ -29,27 +27,29 @@ class Fighter {
         let probabilityAttack = 100 - guard.agility;
         let successfulAttack = Math.round(Math.random() * probabilityAttack);
         if (successfulAttack < probabilityAttack) {
-            console.log(guard.hp + 'HP Guard');
             guard.dealDamage(this.damage);
             console.log(this.name + ' make ' + this.damage + ' damage to ' + guard.name);
         } else {
             console.log(this.name + ' attack missed');
         }
     }
-
     dealDamage(deal) {
         this.hp -= deal;
-
-
     }
 }
-
 const myFighter = new Fighter({name: 'John', damage: 20, hp: 100, agility: 25});
-const myFighter2 = new Fighter({name: 'Jack', damage: 25, hp: 100, agility: 15});
+const myFighter2 = new Fighter({name: 'Jack', damage: 40, hp: 100, agility: 15});
 
 function battle(Fighter1, Fighter2) {
-    while (Fighter2.hp > 0) {
+    if(Fighter1.hp === 0){
+        console.log(Fighter1.name+ ' dead ');
+    }else if(Fighter2.hp === 0){
+        console.log(Fighter2.name+' dead ');
+    }else{
+    while (Fighter1.hp > 0 && Fighter2.hp > 0) {
         Fighter1.attack(Fighter2);
+        Fighter2.attack(Fighter1);
+    }
     }
 }
 
