c7d06e14aec78bf68638a70c5b97c681c0eb654f
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-19 17:35:38 +0300

    add all methods and eslint  test passed

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -5,6 +5,8 @@ class Fighter {
         this.name = data.name;
         this.damage = data.damage;
         this.agility = data.agility;
+        this.win = 0;
+        this.loss = 0;
     }
 
     getName() {
@@ -33,24 +35,56 @@ class Fighter {
             console.log(this.name + ' attack missed');
         }
     }
+
+    logCombatHistory() {
+        return console.log('Name: ' + this.name + ', Wins: ' + this.win + ', Losses: ' + this.loss);
+    }
+
+    heal(heal) {
+        if (this.hp + heal > 100) {
+             this.hp = 100;
+        } else {
+             this.hp += heal;
+        }
+    }
     dealDamage(deal) {
-        this.hp -= deal;
+        if (this.hp > deal) {
+            this.hp = 0;
+        } else {
+            this.hp -= deal;
+        }
+    }
+    addWin() {
+        this.win++;
+    }
+    addLoss() {
+        this.win++;
     }
 }
+
 const myFighter = new Fighter({name: 'John', damage: 20, hp: 100, agility: 25});
 const myFighter2 = new Fighter({name: 'Jack', damage: 40, hp: 100, agility: 15});
 
 function battle(Fighter1, Fighter2) {
-    if(Fighter1.hp === 0){
-        console.log(Fighter1.name+ ' dead ');
-    }else if(Fighter2.hp === 0){
-        console.log(Fighter2.name+' dead ');
-    }else{
-    while (Fighter1.hp > 0 && Fighter2.hp > 0) {
-        Fighter1.attack(Fighter2);
-        Fighter2.attack(Fighter1);
-    }
+    if (Fighter1.hp === 0) {
+        console.log(Fighter1.name + ' dead ');
+    } else if (Fighter2.hp === 0) {
+        console.log(Fighter2.name + ' dead ');
+    } else {
+        while (Fighter1.hp > 0 && Fighter2.hp > 0) {
+            Fighter1.attack(Fighter2);
+            Fighter2.attack(Fighter1);
+        }
+        if (Fighter1.hp > Fighter2.hp) {
+            Fighter1.addWin();
+            Fighter2.addLoss();
+        } else {
+            Fighter1.addLoss();
+            Fighter2.addWin();
+        }
     }
 }
 
 battle(myFighter, myFighter2);
+myFighter.logCombatHistory();
+myFighter.heal(50);
