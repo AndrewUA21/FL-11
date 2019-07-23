13f8c0f38220d18789da063f3c4d9a1baa4ace61
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-23 22:58:50 +0300

    test passed have 2 warning

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,5 +1,5 @@
 let rootNode = document.getElementById('root');
-
+let dragged;
 function addListItem(task) {
     let list = document.createElement('li');
     list.className = 'list-item';
@@ -47,3 +47,93 @@ function deleteTask(e) {
     e.stopPropagation();
 }
 
+function edit(e) {
+    let eventEdit = e.target;
+    if (eventEdit.className === 'material-icons edit') {
+        let li = eventEdit.parentNode;
+        let liBuffer = li;
+        let list = document.createElement('li');
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'replace-task';
+        list.appendChild(input);
+        list.innerHTML += '<i class="material-icons save">save</i>';
+        li.parentNode.replaceChild(list, li);
+        let saveButton = document.querySelector('.save');
+        saveButton.addEventListener('click', () => {
+            let newTask = document.querySelector('.replace-task').value;
+            if (newTask !== '') {
+                liBuffer.children[1].innerText = newTask;
+                list.parentNode.replaceChild(liBuffer, list);
+            }
+        });
+    }
+    e.stopPropagation();
+}
+
+function changeTask(e) {
+    deleteTask(e);
+    edit(e);
+}
+document.querySelector('.list').addEventListener('click', changeTask);
+
+function checked(e) {
+    let check = e.target;
+    if (check.checked) {
+        check.setAttribute('disabled', 'disabled');
+    }
+}
+document.querySelector('.list').addEventListener('change', checked);
+
+
+function dragStart(e) {
+    e.dataTransfer.effectAllowed = 'move';
+    if (e.target.className === 'list-item') {
+        dragged = e.target;
+    }
+}
+document.querySelector('.list').addEventListener('drag', dragStart);
+
+function setBorder(e) {
+    if (e.target.className === 'list-item') {
+        e.target.style.background = '#E2F1FD';
+    }
+}
+document.querySelector('.list').addEventListener('dragenter',setBorder);
+
+function deleteBorder(e) {
+    if (e.target.className === 'list-item') {
+        e.target.style.background = '';
+    }
+}
+document.querySelector('.list').addEventListener('dragleave', deleteBorder);
+
+function dropEnd(e) {
+    e.preventDefault();
+    e.target.style.background = '';
+    dragged.parentNode.removeChild(dragged);
+    let parent = e.target.parentNode;
+    if (parent.tagName === 'UL' && e.target !== parent.lastChild && e.target.className === 'list-item') {
+        parent.insertBefore(dragged, e.target);
+    } else {
+        if (parent.tagName === 'UL' && e.target.className === 'list-item') {
+            parent.insertBefore(dragged, e.target.nextSibling);
+        }
+    }
+}
+document.querySelector('.list').addEventListener('drop', dropEnd);
+
+function setMoveCursore (e){
+    event.preventDefault();
+    if (e.target.className === 'list-item' && e.target.className !== 'task') {
+        e.target.style.cursor = 'move';
+    } else {
+        if (e.target.className === 'task') {
+            e.target.style.cursor = 'default';
+        }
+    }
+}
+document.querySelector('.list').addEventListener('mouseover', setMoveCursore);
+document.querySelector('.list').addEventListener('dragover', function (event){
+    event.preventDefault();
+});
