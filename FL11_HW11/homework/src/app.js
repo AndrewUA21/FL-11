1b4540ef8daa2bbc36fb0465e4350641a3c78114
Andrew <kozhemyakin.andrej@gmail.com>
2019-07-23 21:48:21 +0300

    add create taskgr

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,1 +1,37 @@
 let rootNode = document.getElementById('root');
+
+function addListItem(task) {
+    let list = document.createElement('li');
+    list.className = 'list-item';
+    list.setAttribute('draggable', 'true');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    list.appendChild(checkbox);
+    list.appendChild(task);
+    list.innerHTML += '<i class="material-icons edit">edit</i>';
+    list.innerHTML += '<i class="material-icons delete">delete</i>';
+    return list;
+}
+function inputData() {
+    let inputText = document.getElementById('input-task').value;
+    console.log(inputText);
+    let span = document.createElement('span');
+    span.innerText = inputText;
+    return span;
+}
+function createTask() {
+    let ListItems = document.querySelectorAll('.list-item');
+    if (ListItems.length < 10 && document.getElementById('input-task').value !== '') {
+        let task = inputData();
+        let list = addListItem(task);
+        document.querySelector('.list').appendChild(list);
+        document.getElementById('input-task').value = '';
+    } else if (document.getElementById('input-task').value === '') {
+        return false;
+    } else {
+        document.querySelector('.message').innerText = 'Maximum item per list are created';
+        document.getElementById('input-task').value = '';
+    }
+}
+
+
